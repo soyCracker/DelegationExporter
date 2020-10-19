775a53da07d9d5369e0f3eed8c4ca3f6c8f29fde
yuhui <yuhui.lai.concrete>
2020-10-19 18:07:20 +0800

    real time clock

--- DelegationExporterWeb/Controllers/ExController.cs
+++ DelegationExporterWeb/Controllers/ExController.cs
@@ -2,18 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using DelegationExporterWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DelegationExporterWeb.Controllers
 {
-    public class TableController : Controller
+    public class ExController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
 
-        
+        public IActionResult Clock()
+        {
+            return View();
+        }
+
+        public IActionResult Table()
+        {
+            return View();
+        }
     }
 }
